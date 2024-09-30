2b7cd63fb7a897505655f8d30616a084b5255746
Edmondio <83596559+Edmondio@users.noreply.github.com>
2024-09-30 16:10:26 +0200

    add item

---
+++ items.lua
@@ -0,0 +1,7 @@
+['gofast_bag'] = {
+    label = 'Sac Go-Fast',
+    weight = 1,
+    stack = false,
+    close = true,
+    description = 'Un sac contenant de la drogue pour une mission Go-Fast',
+},
